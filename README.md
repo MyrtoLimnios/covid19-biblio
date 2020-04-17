da06effec1eb3dba097708a1cf46caddf92ce167
Amir Dib <dib.amir@gmail.com>
2020-04-17 15:47:04 +0200

    Implements data pipeline from gsheet to ES

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # covid19-biblio
 
 State-of-the-art on epidemic models for COVID-19.
+
+
+
+## Data Pipe
+The code pull data from google sheet and push it on an Elastic Search Cluster. All hosts/adresses are set in config.json.
+
+## Run
+source inside your virtual environnement then:
+```bash
+pip install -r requirements.txt
+python server.py
+```
---
+++ es_insert.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from utils import read_json, connect_gs, es_write_dataframe
+
+config = read_json("config.json")
+gsheet_name = config["gsheet_name"]
+elasticsearchServer = config["elasticsearchServer"]
+
+
+def clean(dataframe):
+    dataframe['Date of publication'] = pd.to_datetime(
+        dataframe['Date of publication'] + '/2020', format="%m/%d/%Y", errors='coerce')
+
+    code_available_mask = ((dataframe['Code available'] != 'no') & (
+        dataframe['Code available'] != ''))
+    dataframe.loc[code_available_mask, 'bool_code_available'] = True
+    dataframe['bool_code_available'] = dataframe['bool_code_available'].fillna(
+        False)
+    dataframe.loc[code_available_mask, 'str_code_available'] = 'Yes'
+    dataframe['str_code_available'] = dataframe['str_code_available'].fillna(
+        'No')
+    return dataframe
+
+
+if __name__ == '__main__':
+
+    dataframe = load_gsheet(name=gsheet_name,
+                            credentials='credentials.json')
+    dataframe = clean(dataframe)
+    print(dataframe.head(5))
+    responses = es_write_dataframe(
+        dataframe, host=elasticsearchServer, index='catalog')
+
+    if False:
+        print(responses)
